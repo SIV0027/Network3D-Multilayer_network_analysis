7e64d65f03aa28beba54d70f9958efdddebee109
AndrewSiwi <andrew.siwi@seznam.cz>
2026-02-19 16:54:53 +0100

    visualization - multiplexNetwork - test

--- network3D/tests/visualization/multiplex.ts
+++ network3D/tests/visualization/multiplex.ts
@@ -28,47 +28,60 @@ const network = new Network3D.Core.MultiplexNetwork({
     }
 });
 
+const minSize = 15;
+const maxSize = 30;
+
 const visualization = Network3D.Visualization.MultiplexNetwork.create({
     network,
     layerId: "first",
     init: Network3D.Visualization.MultiplexNetwork.init({
         container: "container",
         attrs: {
-            /*
-                        node: {
-              type: "rect",
-              style: {
-                width: 220,
-                height: 80,
-                radius: 16,
-                fill: "rgba(30,41,59,0.8)",
-                stroke: "rgba(148,163,184,0.3)",
-                backdropFilter: "blur(6px)",
-              },
-              labelStyle: {
-                fill: "#f1f5f9",
-                fontSize: 14,
-                fontWeight: 500,
-              },
-              state: {
-                hover: {
-                  stroke: "#38bdf8",
-                  lineWidth: 2,
-                },
-              },
-            }, */
+           node: {
+                type: "circle",
+                style: {
+                    size: maxSize,
+                    fill: "#10B981",
+                    stroke: "#065F46",
+                    lineWidth: 2
+                }
+            },
+            layout: {
+                type: "d3-force",
+                collide: {
+                    radius: maxSize * 1.5,
+                    strength: 1
+                }
+            }
         }
     })
 });
 
 visualization.render();
 
-/*visualization.modify((graph) => {
-    graph.setNode({
-        style: {
-            type: 'circle',
-            style: { size: 20, fill: '#7FFFD4', stroke: '#5CACEE', lineWidth: 2 },
-        },
+setTimeout(() => {
+    visualization.modify((graph) => {
+        graph.setNode({
+            type: "circle",
+                style: {
+                    size: 150,
+                    fill: "#10B981",
+                    stroke: "#065F46",
+                    lineWidth: 2,
+                    labelText: (node: any) => `${node.id}`,
+                    labelPlacement: "center",
+                    labelFontSize: 225 / 5,
+                    labelFill: "#ffffff"
+                }
+        });
+
+        graph.setLayout({
+            type: "d3-force",
+                collide: {
+                    radius: 225,
+                    strength: 1
+                }
+        });
+        visualization.render();
     });
-    visualization.render();
-});*/
+}, 5000);
